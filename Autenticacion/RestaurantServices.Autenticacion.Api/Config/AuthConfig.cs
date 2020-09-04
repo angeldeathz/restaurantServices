b4dcd40ee135a9cc96c74255c9be4145dee8333e
David Moya M <dav.moyam@gmail.com>
2020-09-03 23:24:14 -0400

    - se crea commit inicial

---
+++ Autenticacion/RestaurantServices.Autenticacion.Api/Config/AuthConfig.cs
@@ -0,0 +1,39 @@
+﻿using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.Owin.Security.OAuth;
+
+namespace RestaurantServices.Autenticacion.Api.Config
+{
+    public class AuthConfig : OAuthAuthorizationServerProvider
+    {
+        public override async Task ValidateClientAuthentication(OAuthValidateClientAuthenticationContext context)
+        {
+            await Task.Run(() => { context.Validated(); });
+        }
+
+        public override async Task GrantResourceOwnerCredentials(OAuthGrantResourceOwnerCredentialsContext context)
+        {
+            await Task.Run(() =>
+            {
+                context.OwinContext.Response.Headers.Add("Access-Control-Allow-Origin", new[] {"*"});
+
+                //using (AuthRepository _repo = new AuthRepository())
+                //{
+                //    IdentityUser user = await _repo.FindUser(context.UserName, context.Password);
+
+                //    if (user == null)
+                //    {
+                //        context.SetError("invalid_grant", "The user name or password is incorrect.");
+                //        return;
+                //    }
+                //}
+
+                var identity = new ClaimsIdentity(context.Options.AuthenticationType);
+                identity.AddClaim(new Claim("sub", context.UserName));
+                identity.AddClaim(new Claim("role", "user"));
+
+                context.Validated(identity);
+            });
+        }
+    }
+}
---
+++ Autenticacion/RestaurantServices.Autenticacion.Api/Startup.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Web.Http;
+using Microsoft.Owin;
+using Microsoft.Owin.Cors;
+using Microsoft.Owin.Security.OAuth;
+using Owin;
+using RestaurantServices.Autenticacion.Api;
+using RestaurantServices.Autenticacion.Api.Config;
+
+[assembly: OwinStartup(typeof(Startup))]
+namespace RestaurantServices.Autenticacion.Api
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            //var config = new HttpConfiguration();
+            //WebApiConfig.Register(config);
+            //app.UseWebApi(config);
+            //ConfigureOAuth(app);
+            // Allow all origins
+            //app.UseCors(Microsoft.Owin.Cors.CorsOptions.AllowAll);
+
+            var config = new HttpConfiguration();
+            ConfigureOAuth(app);
+            WebApiConfig.Register(config);
+            app.UseCors(CorsOptions.AllowAll);
+            app.UseWebApi(config);
+        }
+
+        public void ConfigureOAuth(IAppBuilder app)
+        {
+            var oAuthServerOptions = new OAuthAuthorizationServerOptions()
+            {
+                AllowInsecureHttp = true,
+                TokenEndpointPath = new PathString("/token"),
+                AccessTokenExpireTimeSpan = TimeSpan.FromDays(1),
+                Provider = new AuthConfig()
+            };
+
+            // Token Generation
+            app.UseOAuthAuthorizationServer(oAuthServerOptions);
+            app.UseOAuthBearerAuthentication(new OAuthBearerAuthenticationOptions());
+        }
+    }
+}
