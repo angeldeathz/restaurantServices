0391fd5ec74e394224d1b07e99265665db9bdbd9
David Moya M <dav.moyam@gmail.com>
2020-09-12 17:46:11 -0300

    - se agrega api de usuarios

---
+++ Usuarios/RestaurantServices.Usuarios.Api/Controllers/AutenticarController.cs
@@ -0,0 +1,20 @@
+﻿using System.Web.Http;
+
+namespace RestaurantServices.Usuarios.Api.Controllers
+{
+    [Route("api/personas/autenticar")]
+    public class AutenticarController : ApiController
+    {
+        [HttpGet, Authorize]
+        public IHttpActionResult Algo()
+        {
+            var a = new
+            {
+                algo = "Prueba",
+                nombre = "david"
+            };
+
+            return Ok(a);
+        }
+    }
+}
---
+++ Usuarios/RestaurantServices.Usuarios.Api/Startup.cs
@@ -0,0 +1,41 @@
+﻿using System.Web.Http;
+using Microsoft.Owin;
+using Microsoft.Owin.Cors;
+using Microsoft.Owin.Security.OAuth;
+using Owin;
+using RestaurantServices.Usuarios.Api;
+
+[assembly: OwinStartup(typeof(Startup))]
+namespace RestaurantServices.Usuarios.Api
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            var config = new HttpConfiguration();
+            //ConfigureOAuth(app);
+            app.UseExternalSignInCookie();
+            var oAuthServerOptions = new OAuthBearerAuthenticationOptions();
+            app.UseOAuthBearerAuthentication(oAuthServerOptions);
+
+            WebApiConfig.Register(config);
+            app.UseCors(CorsOptions.AllowAll);
+            app.UseWebApi(config);
+        }
+
+        //public void ConfigureOAuth(IAppBuilder app)
+        //{
+        //    var oAuthServerOptions = new OAuthAuthorizationServerOptions
+        //    {
+        //        AllowInsecureHttp = true,
+        //        TokenEndpointPath = new PathString("/token"),
+        //        AccessTokenExpireTimeSpan = TimeSpan.FromDays(1),
+        //        Provider = new AuthConfig()
+        //    };
+
+        //    // Token Generation
+        //    app.UseOAuthAuthorizationServer(oAuthServerOptions);
+        //    app.UseOAuthBearerAuthentication(new OAuthBearerAuthenticationOptions());
+        //}
+    }
+}
