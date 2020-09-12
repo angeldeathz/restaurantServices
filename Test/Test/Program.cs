c8d850ce18cabb4730592ca7ee96322091b5e3da
David Moya M <dav.moyam@gmail.com>
2020-09-12 20:40:59 -0300

    - se crean metodos para insertar y obtener datos desde oracle

---
+++ Test/Test/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RestaurantServices.Shared.Transversal.Repositorio;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var repo = new OracleRepository();
+            var param = new Dictionary<string, object>
+            {
+                {"@ID_PRUEBA", 6},
+                {"@NOMBRE", "sdfsdfsdfs"}
+            };
+            var c = repo.InsertAsync<object>("INSERT_PRUEBA", param, CommandType.StoredProcedure).Result;
+        }
+    }
+}
---
+++ Usuarios/RestaurantServices.Usuarios.DAL/UsuarioDal.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RestaurantServices.Shared.Transversal.Repositorio;
+
+namespace RestaurantServices.Usuarios.DAL
+{
+    public class UsuarioDal
+    {
+        private OracleRepository _repository;
+
+        public UsuarioDal(OracleRepository repository)
+        {
+            _repository = repository;
+        }
+    }
+}
