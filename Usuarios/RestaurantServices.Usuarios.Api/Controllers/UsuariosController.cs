0b24475e0d700194d9f2383def6a4b522b07ca82
David Moya M <dav.moyam@gmail.com>
2020-09-12 20:44:46 -0300

    - se borran referencias q no se utilizaran, mas un fix a api usuarios

--- Usuarios/RestaurantServices.Usuarios.Api/Controllers/UsuariosController.cs
+++ Usuarios/RestaurantServices.Usuarios.Api/Controllers/UsuariosController.cs
@@ -3,10 +3,10 @@
 namespace RestaurantServices.Usuarios.Api.Controllers
 {
     [Route("")]
-    public class AutenticarController : ApiController
+    public class UsuariosController : ApiController
     {
         [HttpGet]
-        public IHttpActionResult ValidarSesion([FromUri] string rut, string contrasena)
+        public IHttpActionResult ValidarSesion([FromUri] string rut, [FromUri] string contrasena)
         {
             if (rut == "a" && contrasena == "b")
             {
