89804e823a89cee842b6a78a5ee9a74779339545
David Moya M <dav.moyam@gmail.com>
2020-09-23 00:03:54 -0300

    - se crea clase para manejo de errores, falta implementacion

---
+++ Shared/RestaurantServices.Shared.Transversal/WebApiConfig/ErrorHandler.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Http.ExceptionHandling;
+
+namespace RestaurantServices.Shared.Transversal.WebApiConfig
+{
+    public class ErrorHandler : ExceptionHandler
+    {
+        public override void Handle(ExceptionHandlerContext context)
+        {
+            base.Handle(context);
+        }
+    }
+}
