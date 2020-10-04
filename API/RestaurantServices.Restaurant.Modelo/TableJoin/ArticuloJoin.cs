2aedddaa31171f2cdf411009a56389e3efa97491
David Moya M <dav.moyam@gmail.com>
2020-10-04 19:43:21 -0300

    - se agrega mas data a articulo

---
+++ API/RestaurantServices.Restaurant.Modelo/TableJoin/ArticuloJoin.cs
@@ -0,0 +1,14 @@
+﻿namespace RestaurantServices.Restaurant.Modelo.TableJoin
+{
+    public class ArticuloJoin
+    {
+        public int IdArticulo { get; set; }
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+        public int Precio { get; set; }
+        public int IdEstadoArticulo { get; set; }
+        public string NombreEstadoArticulo { get; set; }
+        public int IdTipoConsumo { get; set; }
+        public string NombreTipoConsumo { get; set; }
+    }
+}
