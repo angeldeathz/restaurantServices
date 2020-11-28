bc0fdc8aa1d47e0bfade92465214adfa60daa3e9
David Moya M <dav.moyam@gmail.com>
2020-11-27 22:51:42 -0300

    - se crea controller de reportes

---
+++ API/RestaurantServices.Restaurant.Api/Controllers/ReportesController.cs
@@ -0,0 +1,21 @@
+﻿using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Description;
+
+namespace RestaurantServices.Restaurant.API.Controllers
+{
+    [Authorize, RoutePrefix("api/reportes")]
+    public class ReportesController : ApiController
+    {
+        [HttpGet, Route("{id}")]
+        [ResponseType(typeof(string))]
+        public async Task<IHttpActionResult> Get(int id)
+        {
+            var reporteBase64 = string.Empty;
+            if (reporteBase64 == null) return ResponseMessage(new HttpResponseMessage(HttpStatusCode.NoContent));
+            return Ok(reporteBase64);
+        }
+    }
+}
