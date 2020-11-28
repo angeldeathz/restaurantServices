ea39868397eb20eff6a10931edc88dc7012c641a
David Moya M <dav.moyam@gmail.com>
2020-11-27 22:37:05 -0300

    - se refactoriza metodo que obtiene un html a pdf en formato base64

--- API/RestaurantServices.Restaurant.Shared/Itextsharp/ItextSharpClient.cs
+++ API/RestaurantServices.Restaurant.Shared/Itextsharp/ItextSharpClient.cs
@@ -1,27 +1,11 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using iText.Html2pdf;
 
 namespace RestaurantServices.Restaurant.Shared.Itextsharp
 {
     public class ItextSharpClient
     {
-//        public string CreatePdf2(string html, string fileName)
-//        {
-//            var path = CrearCarpetaStorage();
-//            path = $"{path}\\{fileName}";
-
-//            var file = new FileStream(path, FileMode.Create);
-//            var document = new Document();
-//            PdfWriter.GetInstance(document, file);
-//            document.Open();
-//#pragma warning disable 612
-//            var hw = new HTMLWorker(document);
-//#pragma warning restore 612
-//            hw.Parse(new StringReader(html));
-//            document.Close();
-//            return path;
-//        }
-
         public string CreatePdf(string html, string fileName)
         {
             var path = CrearCarpetaStorage();
@@ -31,38 +15,23 @@ namespace RestaurantServices.Restaurant.Shared.Itextsharp
             HtmlConverter.ConvertToPdf(html, file);
             file.Close();
 
+            CreatePdfBase64(html);
+
             return path;
         }
 
-//        public string CreatePdfBase64(string html)
-//        {
-//            string base64;
+        public string CreatePdfBase64(string html)
+        {
+            string base64;
 
-//            using (var msOutput = new MemoryStream())
-//            {
-//                var reader = new StringReader(html);
-//                var document = new Document(PageSize.A4, 30, 30, 30, 30);
-//                PdfWriter.GetInstance(document, msOutput);
-
-//#pragma warning disable 612
-//                var worker = new HTMLWorker(document);
-//#pragma warning restore 612
-
-//                document.Open();
-//                worker.StartDocument();
-
-//                worker.Parse(reader);
-//                worker.EndDocument();
-//                worker.Close();
-//                document.Close();
-
-//                var bytes = msOutput.ToArray();
-//                base64 = Convert.ToBase64String(bytes);
-//                document.Close();
-//            }
-
-//            return base64;
-//        }
+            using (var msOutput = new MemoryStream())
+            {
+                HtmlConverter.ConvertToPdf(html, msOutput);
+                var bytes = msOutput.ToArray();
+                base64 = Convert.ToBase64String(bytes);
+            }
+            return base64;
+        }
 
         public string CrearCarpetaStorage()
         {
